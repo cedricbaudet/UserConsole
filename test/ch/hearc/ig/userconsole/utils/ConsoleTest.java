e0fb4898c612c8f74e65cdd9e2ec4f2220bf899a
cedricbaudet <cedric.baudet@he-arc.ch>
2024-10-10 15:57:53 +0200

    Test userconsole (unit test)

--- test/ch/hearc/ig/userconsole/utils/ConsoleTest.java
+++ test/ch/hearc/ig/userconsole/utils/ConsoleTest.java
@@ -9,8 +9,8 @@ class ConsoleTest {
     @Test
     void test() {
         Console console = new Console();
-        console.setEntry("First entry");
-        console.setEntry("Second entry");
+        console.setEntry("First entry (1)");
+        console.setEntry("Second entry (2)");
         assert (!console.getEntries().isEmpty());
         console.printMenu();
     }
